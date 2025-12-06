d73c0220499aea3528023b69eae0d19ffdc244d8
Mykolas Budrys <mykolas.budrys@gmail.com>
2025-12-06 20:39:28 +0200

    github pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,19 @@
 import {StrictMode} from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {HashRouter,Routes,Route} from 'react-router-dom'
 import {createRoot} from 'react-dom/client'
 import './main.css'
 import App from './App.jsx'
 
 import {ThemeProvider} from './utils/themeContext.jsx'
 
+const base = import.meta.env.VITE_BASE_URL
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter basename={base}>
       <ThemeProvider>
         <App/>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
