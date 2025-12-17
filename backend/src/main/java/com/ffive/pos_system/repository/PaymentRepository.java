f0e73290a50cbb468d93f0356c3d33cefb00b809
elvinukas <elvinas.svilpa@mif.stud.vu.lt>
2025-12-17 22:19:56 +0200

    Added actual db .xml files of Payment tables, added the repository queries.

--- backend/src/main/java/com/ffive/pos_system/repository/PaymentRepository.java
+++ backend/src/main/java/com/ffive/pos_system/repository/PaymentRepository.java
@@ -4,9 +4,15 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ffive.pos_system.model.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-    List<Payment> findByOrderId(UUID orderId);
+    @Query("""
+            SELECT p FROM Payment p
+            WHERE p.orderId = :orderId
+            """)
+    List<Payment> findByOrderId(@Param("orderId") UUID orderId);
 }
--- backend/src/main/java/com/ffive/pos_system/repository/SplitCheckRepository.java
+++ backend/src/main/java/com/ffive/pos_system/repository/SplitCheckRepository.java
@@ -1,11 +1,25 @@
 package com.ffive.pos_system.repository;
 
+import com.ffive.pos_system.model.Order;
 import com.ffive.pos_system.model.SplitCheck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface SplitCheckRepository extends JpaRepository<SplitCheck, UUID> {
-    List<SplitCheck> findByOrderId(UUID orderId);
+    @Query("""
+            SELECT sc FROM SplitCheck sc
+            WHERE sc.order.id = :orderId
+            """)
+    List<SplitCheck> findByOrderId(@Param("orderId") UUID orderId);
+
+    @Query("""
+            SELECT sc FROM SplitCheck sc
+            WHERE sc.order = :order
+            """)
+    List<SplitCheck> findByOrder(@Param("order") Order order);
 }
 
